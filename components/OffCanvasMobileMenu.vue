c6b324e864b50294b2b321ce6f34a4b16c85746a
noel <nrio2864@gmail.com>
2022-07-01 08:52:23 +0700

    init

---
+++ components/OffCanvasMobileMenu.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="popup-mobile-menu popup-mobile-visiable" id="offcanvas-menu">
+        <div class="mobile-menu-overlay" @click="mobiletoggleClass('removeClass', 'show-mobile-menu')"></div>
+        <div class="inner custom-scrollbar">
+            <div class="mobile-header">
+                <div class="logo">
+                    <n-link to="/" style="
+    color: white;">
+                  <!-- Yohanes Gabriel -->
+                   <img class="dark-logo" src="/images/logo/yoga-logo.png" alt="Agency Logo">
+                </n-link>
+                </div>
+                <button class="mobile-close-btn" @click="mobiletoggleClass('removeClass', 'show-mobile-menu')"></button>
+            </div>
+            <div class="menu-content">
+                <MobileNavigation />
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        components: {
+            MobileNavigation: () => import('@/components/MobileNavigation'),
+        },
+        
+        methods: {
+            // offcanvas menu close
+            mobiletoggleClass(addRemoveClass, className) {
+                const el = document.querySelector('#offcanvas-menu');
+                if (addRemoveClass === 'addClass') {
+                    el.classList.add(className);
+                } else {
+                    el.classList.remove(className);
+                }
+            }
+        }
+    };
+</script>
